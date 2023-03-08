fad8bde6b4f8f7dfdf998003062f54e99cfcd089
AkankshaSomayaji <somayajimiracle@gmail.com>
2023-03-09 01:50:30 +0530

    page breaks and headings

--- src/pages/hmyehy/constants.js
+++ src/pages/hmyehy/constants.js
@@ -1,13 +1,6 @@
 export const title = "How to Make Your Employees Hate You : An Idiot's Guide to Management";
 export const subtitle = "Creating a Toxic Work Environment, One Bad Decision at a Time";
-
-export const intro_title = "How to Embrace Your Inner Jerk";
-export const intro_content = <>
-    Welcome to "How to Make Your Employees Hate You: An Idiot's Guide to Management"! Congratulations on taking the first step towards becoming the world's worst boss! If you're reading this book, chances are you're already on the right track to destroying workplace morale and driving your employees to the brink of insanity.
-    But don't worry, this guidebook is here to help you take your terrible management skills to the next level! Whether you're a seasoned pro at making your employees hate you, or a newbie looking to up your game, this book has everything you need to succeed.
-    In these pages, you'll learn the art of micromanagement, how to communicate poorly, and why it's always better to criticize than to praise. We'll cover everything from toxic work environments to demotivating your employees, so you can truly be the worst boss ever.
-    So buckle up, grab a cup of coffee (and don't offer any to your employees), and let's get started on this journey to becoming the most hated manager in the world!
-</>
+export const page_header = "An Idiot's Guide to Management";
 
 export const dedication_content = <>
     To all the managers who have mastered the art of making their employees hate them, this book is dedicated to you.
@@ -17,6 +10,149 @@ export const dedication_content = <>
     This book is for the managers who have a degree in business, but no clue how to run a business. Your employees definitely think you're an idiot.
     Here's to all the terrible bosses out there.
 </>
+
+export const toc_title = "Contents";
+export const toc_content_p1 = <>
+    <p>Introduction: How to Embrace Your Inner Jerk</p>
+
+    <p>Chapter 1: The Art of Micromanagement: When in Doubt, Breathe Down Their Necks</p>
+
+    <p>Chapter 2: Demotivation 101: How to Crush Employee Morale in 3 Easy Steps</p>
+
+    <p>Chapter 3: The Power of Negativity: When Compliments Are Just a Waste of Time</p>
+
+    <p>Chapter 4: Communication Breakdown: Talking Without Actually Saying Anything</p>
+
+    <p>Chapter 5: Why be a leader when you can be a boss: How to Criticize Without Offering Solutions</p>
+
+    <p>Chapter 6: The Art of the Insult: How to Criticize Like a Pro</p>
+
+    <p>Chapter 7: Creating a Toxic Workplace: A Guide to Making Your Employees Miserable</p>
+
+    <p>Chapter 8: Conflict Resolution: When All Else Fails, Blame Your Employees</p>
+</>
+
+export const toc_content_p2 = <>
+    <p>Chapter 9: The Lazy Manager's Guide to Delegation: How to Do Nothing and Get Away with It</p>
+
+    <p>Chapter 10: Work-Life Imbalance: How to Make Sure Your Employees Never Leave the Office</p>
+
+    <p>Chapter 11: The Importance of Nepotism: How to Hire Your Family and Friends</p>
+
+    <p>Chapter 12: The Power of Inconsistency: How to Keep Your Employees on Their Toes</p>
+
+    <p>Chapter 13: Ethics Schmethics: How to Ignore the Rules and Get Ahead</p>
+
+    <p>Chapter 14: Breaking the Rules: How to Set a Terrible Example for Your Team</p>
+
+    <p>Chapter 15: When Good Intentions Go Bad: The Art of Making Terrible Decisions</p>
+
+    <p>Conclusion: Embracing Your Incompetence and Owning Your Terrible Management Skills.</p>
+</>
+
+export const intro_title = "How to Embrace Your Inner Jerk";
+export const intro_content = <>
+    Welcome to "How to Make Your Employees Hate You: An Idiot's Guide to Management"! Congratulations on taking the first step towards becoming the world's worst boss! If you're reading this book, chances are you're already on the right track to destroying workplace morale and driving your employees to the brink of insanity.
+    But don't worry, this guidebook is here to help you take your terrible management skills to the next level! Whether you're a seasoned pro at making your employees hate you, or a newbie looking to up your game, this book has everything you need to succeed.
+    In these pages, you'll learn the art of micromanagement, how to communicate poorly, and why it's always better to criticize than to praise. We'll cover everything from toxic work environments to demotivating your employees, so you can truly be the worst boss ever.
+    So buckle up, grab a cup of coffee (and don't offer any to your employees), and let's get started on this journey to becoming the most hated manager in the world!
+</>
+
+export const chapters_count = 12;
+
+export const chap_titles = [
+    "The Art of Micromanagement: When in Doubt, Breathe Down Their Necks",
+    "Demotivation 101: How to Crush Employee Morale in 3 Easy Steps",
+    "The Power of Negativity: When Compliments Are Just a Waste of Time",
+    "Communication Breakdown: Talking Without Actually Saying Anything",
+    "Why be a leader when you can be a boss: How to Criticize Without Offering Solutions"
+];
+
+export const chap_contents = [
+    [
+        <>
+            Micromanagement - it's a dirty word in the workplace. But why? After all, what could be more fun than constantly hovering over your employees, correcting their every move, and suffocating them with your presence? It's an art form, really, and those who have mastered it know the true joy of being a boss.
+
+            First and foremost, micromanagement is all about control. Who needs autonomous, creative employees when you can have a team of mindless drones who do exactly what you say, when you say it? And let's face it, who wouldn't want to be the puppet master of their own little workforce?
+            <br/><br/>
+            Of course, the key to successful micromanagement is to be as annoying as possible. You don't want your employees to think they can get away with anything, do you? No, you want them to constantly feel the pressure of your breath on their necks, reminding them of who's really in charge.
+
+            And what better way to micromanage than to be constantly monitoring your employees' every move? Install cameras in the break room, tap their phones, and make sure their computer screens are always visible to you. It's not creepy, it's just good management.
+            <br/><br/>
+            But micromanagement isn't just about surveillance -
+        </>,
+        <>
+            it's also about nitpicking. Find every possible mistake your employees make, no matter how insignificant, and make sure they know about it. Did someone forget to capitalize a letter in an email? Send them a lengthy memo about the importance of attention to detail. Did someone take an extra five minutes on their lunch break? Make sure they know they're on thin ice.
+
+            Of course, there's always the risk that your employees will resent you for your micromanaging ways. But fear not! With a little bit of gaslighting, you can make them think that it's all in their heads. When they complain about your constant oversight, just tell them that you're only trying to help them be the best they can be. And if they still aren't convinced, just accuse them of being too sensitive.
+
+            But really, the true art of micromanagement is all about being petty. Did someone forget to say "good morning" to you this morning? Make sure they know that their attitude is unacceptable. Did someone leave a pen out on their desk? Write them up for creating a cluttered workspace. It's not about the pen, it's about the principle.
+            <br/><br/>
+            In conclusion, micromanagement is an underappreciated art form that requires skill, dedication, and a willingness to annoy the heck out of your employees. So the next time you're in doubt about how to manage your team, just remember: when in doubt, breathe down their necks."
+        </>
+    ],
+    [
+        <>
+            Ah, the joys of being an employer. While many believe that employee morale is an essential aspect of running a successful business, others see it as a mere distraction from achieving profits. If you belong to the latter group, fear not! In this guide, we will teach you how to crush your employee's morale in three easy steps.
+            <br/><br/>
+            Step 1: Micromanage Like Your Life Depends On It
+            <br/>
+            Ah, micromanagement, the perfect tool to let your employees know that you don't trust them at all. Who needs autonomy and independence when you can have someone breathing down your neck 24/7? Make sure to check in on your employees every five minutes, and if they haven't completed a task within a nanosecond of receiving it, make sure to berate them in front of everyone. Remember, nothing boosts morale like making someone feel incompetent and worthless.
+            <br/><br/>
+            Step 2: Play Favorites
+            <br/>
+            Why treat everyone equally when you can play favorites? Identify your favorite employees and make sure to give them all the perks and opportunities while ignoring the rest. Make sure to show them off in front of everyone else, too, so that the less fortunate employees know what they are missing out
+        </>,
+        <>
+            on. This is an excellent way to promote resentment and discourage teamwork.
+            <br/><br/>
+            Rest
+        </>
+    ],
+    [
+        <>
+            Compliments are just a waste of time, and let me tell you, I know a thing or two about wasting time. Why bother with all that fluff and positivity when you can just tear someone down? It's like ripping off a band-aid, sure it hurts at first, but it's better in the long run.
+
+            Let's face it, compliments are just a way to coddle employees and make them feel special. But why should they feel special? They're just doing their job, like they're supposed to. If they want recognition, they should do something truly exceptional, like putting on a circus act or climbing Mount Everest without oxygen. Then, maybe I'll think about giving them a pat on the back.
+
+            Besides, compliments are often insincere and just a way to avoid confrontation. Instead of beating around the bush, it's better to just tell someone they're doing a terrible job. Maybe they'll cry a little, but hey, tears are just a sign that you're really making a difference.
+            <br/><br/>
+            And let's not forget that negativity is just more effective. Nothing motivates someone like constantly telling them how much they suck. It's like a personal trainer, but for the soul. Plus, it's fun to watch people squirm.
+
+            So, if you want a workplace that's truly productive, forget about all this touchy-feely compliment nonsense. Negativity is the only way to go.
+            
+            Think about it, when someone receives a compliment, what do they do? They get all happy and complacent, thinking they've reached the peak of their career. But when they receive criticism or negative feedback, that's when they really start to work hard and improve.
+
+            And let's not forget about the power of humiliation. Publicly berating someone in front of their colleagues can really light a fire under their butt. It's like a hazing ritual, but for the office. Sure, they may cry themselves to sleep at night, but at least they'll be more motivated at work.
+            <br/><br/>
+            Plus, when you're constantly negative, it's like having a superpower. You can make people feel small and insignificant with just a few well-placed insults. It's like being a villain in a movie, except you don't have to wear a cape (although, that would be pretty cool).
+
+            Oh boy, participation trophies and positive reinforcement, aren't those just great? Let's just give everyone a pat on the back for showing up and doing the bare minimum. Who needs excellence when we can have mediocrity? Because, you know, the real key to success is making sure everyone feels special, even if they don't deserve it.
+
+            And don't even get me started on positivity. That's just going to make everyone think they're amazing even when they're not. We need negativity to keep people grounded and humble. Because nothing motivates employees more than constantly tearing them down, right?
+
+            So, if you want a workforce that's truly motivated and productive, skip the compliments and focus on the negativity. It may not make you the most popular boss, but at least you'll know you're getting results.
+            <br/><br/>
+            But let's be real, who cares about popularity when you have power? And negativity is a surefire way to establish dominance in the workplace. It shows employees who's boss and keeps them on their toes.
+
+            Sure, some people might argue that positivity can also motivate employees, but that's just a load of hogwash. Positivity is for hippies and people who wear socks with sandals. Negativity is for winners.
+
+            And let's not forget about the power of fear. When employees are constantly afraid of being criticized or publicly humiliated, they'll work harder and be more productive. It's like having a bunch of mice running through a maze, except the mice are your employees and the maze is their job.
+
+            And don't even get me started on self-esteem. Who needs it? It's just a bunch of hot air that makes people feel good about themselves. But when you constantly criticize someone, it keeps their ego in check and makes them realize they're not as great as they think they are. It's like a reality check, but for the soul.
+            <br/><br/>
+            So, in conclusion, compliments are just a waste of time and energy. Negativity is the only way to create a workforce that's truly motivated and productive. Sure, it may not be the most pleasant work environment, but who needs happiness when you have success?
+        </>
+    ],
+    [
+        <>
+        </>
+    ],
+    [
+        <>
+        </>
+    ]
+];
 
 export const glossary_content = <>
     Sample Performance Review: A template for managers to use when evaluating employees. Includes helpful phrases such as "Needs improvement" and "Unacceptable performance".
--- src/pages/hmyehy/hmyehy.js
+++ src/pages/hmyehy/hmyehy.js
@@ -5,6 +5,7 @@ import cover from '../../images/cover.png';
 import bindingBG from '../../images/binding_bg.png';
 import pageBG from '../../images/page_bg.png';
 import * as constants from './constants';
+import './hmyehy.scss';
 
 const PageCover = React.forwardRef((props, ref) => {
     return (
@@ -33,21 +34,37 @@ const PageCoverEnd = React.forwardRef((props, ref) => {
 const Page = React.forwardRef((props, ref) => {
     return (
         <div ref={ref} data-density={props.density | "soft"}>
-            <div style={{ backgroundImage: `url(${pageBG})`, height:"872px", width:"550px", border:"2px solid rgb(198, 194, 194)"}}>
+            <div style={{ backgroundImage: `url(${pageBG})`, height:"872px", width:"550px", border:"2px solid rgb(198, 194, 194)", padding:"20px 20px 10px 20px"}}>
                 {/* box-shadow: 0 1.5em 3em -1em rgb(70, 69, 69); */}
                 {props.chapter ?
-                    <div style={{marginTop:"10px"}}>
-                        <h2><Badge bg="secondary">11</Badge></h2>
-                        <h2>{props.chapter}</h2>
+                    <div>
+                        <h2><Badge bg="secondary">{props.chapterNo}</Badge></h2>
+                        <h2 style={{margin:"15px 0px 40px 0px", fontFamily:"caveat"}}>{props.chapter}</h2>
                     </div>
-                    : ''
+                    : 
+                    <div>
+                        {props.pageNo%2===0 ?
+                            <div className="page_heading">
+                                <p>{props.pageNo}</p>
+                                <p>{constants.page_header}</p>
+                                <hr style={{ backgroundColor: "black", height: "5px" }} />
+                            </div> :
+                            <div className="page_heading">
+                                <p>{constants.page_header}</p>
+                                <p>{props.pageNo}</p>
+                            </div>
+                        }
+                    </div>
                 }
                 {props.quote ?
-                    <h3>{props.quote}</h3>
+                    <h3 style={{marginTop:"10px 0px"}}>{props.quote}</h3>
                     : ''
                 }
-                <div>{props.children}</div>
-                <div>{props.number}</div>
+                <div style={{fontFamily:"monospace"}}>{props.children}</div>  {/*text-align:"justify"*/}
+                {props.chapter ?
+                    <div style={{margin:"20px 0px", position:"absolute", bottom:"0px", left:"270px"}}>{props.pageNo}</div>
+                    : ""
+                }
             </div>
         </div>
     );
@@ -73,7 +90,7 @@ export default class HMYEHY extends React.Component {
 
         pages.push(
             <PageTemplate key="title">
-                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 10px"}}>
+                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 10px", gap:"20px"}}>
                     <h1 style={{fontFamily:"caveat"}}>{constants.title}</h1>
                     <h4 style={{fontFamily:"caveat"}}>{constants.subtitle}</h4>
                 </div>
@@ -81,41 +98,52 @@ export default class HMYEHY extends React.Component {
         );
         pages.push(<PageTemplate key="plain" />);
         pages.push(
-            <PageTemplate key="intro">
-                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 10px"}}>
-                    <h1 style={{fontFamily:"caveat"}}>{constants.intro_title}</h1>
-                    <h8 style={{fontFamily:"monospace"}}>{constants.intro_content}</h8>
-                </div>
-            </PageTemplate>
-        );
-        pages.push(<PageTemplate key="plain" />);
-        pages.push(
             <PageTemplate key="dedication">
-                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 10px"}}>
+                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 20px"}}>
                     <h8 style={{fontFamily:"monospace"}}>{constants.dedication_content}</h8>
                 </div>
             </PageTemplate>
         );
         pages.push(<PageTemplate key="plain" />);
 
-        let i=0;
-    
-        for (i = 0; i < 6; i++) {
-            pages.push(
-                <Page key={i} number={i} chapter={"hello meri jaan"} chapterNo={1}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus
-                    mollis nibh, non convallis ex convallis eu. Suspendisse potenti.
-                    Aenean vitae pellentesque erat. Integer non tristique quam.
-                    Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra
-                    metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus,
-                    accumsan eros sed, viverra enim. Pellentesque non justo vel nibh
-                    sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. In cursus mollis nibh, non convallis ex
-                    convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat.
-                    Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin
-                    mollis, eros velit viverra metus, a venenatis tellus tellus id magna.
-                </Page>
-            );
+        pages.push(
+            <PageTemplate key="toc">
+                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 10px"}}>
+                    <h1 style={{fontFamily:"caveat"}}>{constants.toc_title}</h1>
+                    <h8 style={{fontFamily:"monospace"}}>{constants.toc_content_p1}</h8>
+                </div>
+            </PageTemplate>
+        );
+        pages.push(
+            <PageTemplate key="toc">
+                <div style={{display:"flex", height:"inherit", flexDirection:"column", justifyContent:"center", padding:"0px 10px"}}>
+                    <h8 style={{fontFamily:"monospace"}}>{constants.toc_content_p2}</h8>
+                </div>
+            </PageTemplate>
+        );
+
+        pages.push(<PageTemplate key="plain" />);
+        pages.push(<PageTemplate key="plain" />);
+
+        let page_no=1;
+        //const chapter_no = constants.chapters_count;
+        const chapter_no = 5;
+
+        pages.push(
+            <Page key="intro" pageNo={page_no} chapter={constants.intro_title} chapterNo="Intro">
+                {constants.intro_content}
+            </Page>
+        );
+
+        for (let i = 1; i <= chapter_no; i++) {
+            for (let page_i=0; page_i<constants.chap_contents[i-1].length; page_i++) {
+                page_no+=1;
+                pages.push(
+                    <Page key={""+i+page_i} pageNo={page_no} chapter={page_i===0 ? constants.chap_titles[i-1] : ""} chapterNo={i}>
+                        {constants.chap_contents[i-1][page_i]}
+                    </Page>
+                );
+            }
         }
   
         pages.push(<PageCoverEnd key="behindCover" />);
