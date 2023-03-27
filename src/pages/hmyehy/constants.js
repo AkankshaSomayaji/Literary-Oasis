197157007d88452c9c8b2e2b4c21233e7e8086d1
AkankshaSomayaji <somayajimiracle@gmail.com>
2023-03-27 09:50:33 +0530

    chap 11..

--- src/pages/hmyehy/constants.js
+++ src/pages/hmyehy/constants.js
@@ -407,7 +407,7 @@ export const chap_contents = [
     [ // chap 11 The Importance of Nepotism: How to Hire Your Family and Friends
         <>
             Ah, nepotism - the ultimate hack for creating a workforce that's more like a family reunion. Its the fine art of hiring family and friends to do jobs they're not qualified for. Who needs expertise and experience when you can have loyalty and familiarity, right?
-
+            
             <p className="quotes">"Who needs to build a team of qualified professionals when you can just build a family tree?"</p>
 
             Indubitably, some people might call it unfair or unethical, but who cares about that when you can surround yourself with people who will never question your authority? After all, who needs checks and balances when you have your cousin on speed dial? Sure, some people might call it favoritism, but who cares about that when you can have your bestie by your side at the company picnic? After all, no one cares about professionalism when you can have inside jokes and shared childhood memories.
@@ -415,14 +415,16 @@ export const chap_contents = [
             And why limit yourself to just hiring your immediate family? Expand your social circle to include that random guy you met at the gym last week or your old high school crush. Who knows, maybe they'll surprise
         </>,
         <>
-            And why limit yourself to just hiring your immediate family? Expand your social circle to include that random guy you met at the gym last week or your old high school crush. Who knows, maybe they'll surprise you and actually do a good job. Or maybe they'll just sit around and collect a paycheck, coasting by on their charming personality - either way, it's win-win for you! A great excuse to throw a company party!
+            you and actually do a good job. Or maybe they'll just sit around and collect a paycheck, coasting by on their charming personality - either way, it's win-win for you! A great excuse to throw a company party!
+            
+            <p className="quotes">"It's stupid to bother with meritocracy when you can just rely on your uncle's mediocre work ethic. Duh!"</p>
 
             Plus, think of all the bonding time you'll get with your loved ones. Who needs family vacations when you can spend 9 hours a day together in the office? You'll have plenty of time to catch up on old times, reminisce about the good old days, and plot ways to take over the company together. Think of all the hilarious office pranks you can pull with your friends and family. Worrying absolutely nothing about pesky things like deadlines and productivity when you can just goof off with your loved ones all day? It's like being back in your childhood bedroom, but with better snacks.
 
             And let's not forget about the benefits of nepotism when it comes to workplace drama. Who needs anonymous HR complaints or constructive feedback when you can just have your sister tell you what everyone is saying behind your back? It's like having your own personal spy network!
 
             Beyond doubt, some people might call you a tyrant or a dictator, but that's just because they don't understand the importance of keeping it in the family. Who needs a diverse and inclusive workplace when you can have a cozy little bubble of nepotism?
-
+            <br/><br/>
             So go ahead, hire your sister as the CFO, your uncle as the head of HR, your college roommate as the IT guy and your best friend as the head of marketing. Who needs competence and qualifications or even a month of experience, when you have love and loyalty? It's the perfect recipe for a workplace that's more like a sitcom than a serious business. It's the utltimate mantra for success - or at least a really awkward Thanksgiving dinner.
         </>
     ],
