e6f53adde5bcbcf6ead8841eedc4c9b3f9da045a
AkankshaSomayaji <somayajimiracle@gmail.com>
2023-06-27 01:37:25 +0530

    remove tag

--- src/components/aboutCard.js
+++ src/components/aboutCard.js
@@ -30,12 +30,7 @@ function AboutCard(props) {
                         }
                         <div className="about-comp-description">
                             <div className="about-comp-title">
-                                <p>
-                                    <strong>{props.contentHeader}</strong>
-                                </p>
-                                {props.save &&
-                                    <svg fillRule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style={{mixBlendMode: "normal"}} textAnchor="none" fontSize="none" fontWeight="none" fontFamily="none" strokeDashoffset="0" strokeDasharray="" strokeMiterlimit="10" strokeLinejoin="miter" strokeLinecap="butt" strokeWidth="1" stroke="none" fillRule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg>
-                                }
+                                <strong>{props.contentHeader}</strong>
                             </div>
                             <p className="about-comp-card-footer">
                                 {props.contentFooter}
