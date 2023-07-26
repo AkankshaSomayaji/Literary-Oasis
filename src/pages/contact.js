7c6c84afad985264371cba9133df1e17ec9cd7f7
AkankshaSomayaji <somayajimiracle@gmail.com>
2023-07-27 00:59:14 +0530

    minor

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,9 +6,9 @@ import * as constants from './constants';
 
 function Contact() {
     return (
-        <div className="contact-container">
+        <div>
             <NavBar/>
-            <div style={{backgroundColor:"#eae5df", marginTop:"10px", display:"flex", alignItems:"center", minHeight:"94vh"}}>
+            <div className='contact-body'>
                 <Image
                     className="d-block"
                     src={chatImage}
@@ -17,16 +17,16 @@ function Contact() {
                     height={500}
                 />
                 <div className='contact-code'>
-                    <p>{constants.contact1}</p>
-                    <p>{constants.contact2}</p>
-                    <p>{constants.contact3}</p>
-                    <p style={{paddingLeft:"20px"}}>{constants.contact4}</p>
-                    <p style={{paddingLeft:"20px"}}>{constants.contact5}</p>
-                    <p style={{paddingLeft:"20px"}}>{constants.contact6}</p>
-                    <p style={{paddingLeft:"40px"}}>{constants.contact7}</p>
-                    <p style={{paddingLeft:"40px"}}>{constants.contact8}</p>
-                    <p style={{paddingLeft:"40px"}}>{constants.contact9}</p>
-                    <p style={{paddingLeft:"20px"}}>{constants.contact10}</p>
+                    <text>{constants.contact1}</text>
+                    <text>{constants.contact2}</text>
+                    <text>{constants.contact3}</text>
+                    <text style={{paddingLeft:"20px"}}>{constants.contact4}</text>
+                    <text style={{paddingLeft:"20px"}}>{constants.contact5}</text>
+                    <text style={{paddingLeft:"20px"}}>{constants.contact6}</text>
+                    <text style={{paddingLeft:"40px"}}>{constants.contact7}</text>
+                    <text style={{paddingLeft:"40px"}}>{constants.contact8}</text>
+                    <text style={{paddingLeft:"40px"}}>{constants.contact9}</text>
+                    <text style={{paddingLeft:"20px"}}>{constants.contact10}</text>
                 </div>
             </div>
         </div>
